c65054d919cfab54b18cbd256505e27fe4ff27c1
willsrosa <33320360+willsrosa@users.noreply.github.com>
2022-02-18 15:55:49 -0300

    app.js

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const fs = require('fs');
 const { phoneNumberFormatter } = require('./helpers/formatter');
 const axios = require('axios');
 const port = 2096;
-   var privateKey = fs.readFileSync('selfsigned.key', 'utf8');
-   var certificate = fs.readFileSync('selfsigned.crt', 'utf8');
-   var credentials = { key: privateKey, cert: certificate };
+var privateKey = fs.readFileSync('selfsigned.key', 'utf8');
+var certificate = fs.readFileSync('selfsigned.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 const server = https.createServer(credentials, app);
@@ -114,153 +114,59 @@ const createSession = function (id, description) {
 
 
   client.on('message', async (msg) => {
-    console.log(msg)
-    if (msg.type == "list_response") {
-       if (msg.body == "Sim") {
-        msg.reply('Ok, vamos te passar maiores informações sobre o paciente😃')
-        client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
+    if (!msg.from.toUpperCase().includes("g.us")) {
 
-        if (msg.selectedRowId.toUpperCase().includes("THAIS ALVES")) {
-          client.sendMessage("120363039348257323@g.us", msg.selectedRowId);
+      if (msg.type == "list_response") {
+        if (msg.body == "Sim") {
+          msg.reply('Ok, vamos te passar maiores informações sobre o paciente😃')
+          client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
+
+          if (msg.selectedRowId.toUpperCase().includes("THAIS ALVES")) {
+            client.sendMessage("120363039348257323@g.us", msg.selectedRowId);
+          }
+
+
+          if (msg.selectedRowId.toUpperCase().includes("BEATRIZ NALIM")) {
+            client.sendMessage("120363038291296660@g.us", msg.selectedRowId);
+          }
+
+          if (msg.selectedRowId.toUpperCase().includes("BIANCA NASCIMENTO")) {
+            client.sendMessage("120363039945172091@g.us", msg.selectedRowId);
+          }
+
+          if (msg.selectedRowId.toUpperCase().includes("THAIS RODRIGUES")) {
+            client.sendMessage("120363024013484590@g.us", msg.selectedRowId);
+          }
+
+          if (msg.selectedRowId.toUpperCase().includes("JULIANA SOUZA")) {
+            client.sendMessage("120363040378100634@g.us", msg.selectedRowId);
+          }
+          if (msg.selectedRowId.toUpperCase().includes("ANNE OLIVEIRA")) {
+            client.sendMessage("120363039377213562@g.us", msg.selectedRowId);
+          }
+          if (msg.selectedRowId.toUpperCase().includes("ANNA BEATRIZ")) {
+            client.sendMessage("120363038774243623@g.us", msg.selectedRowId);
+          }
+          if (msg.selectedRowId.toUpperCase().includes("BENE SOUZA")) {
+            client.sendMessage("120363023390637872@g.us", msg.selectedRowId);
+          }
+          if (msg.selectedRowId.toUpperCase().includes("HAYNE SEJANI")) {
+            client.sendMessage("120363023276101126@g.us", msg.selectedRowId);
+          }
+          if (msg.selectedRowId.toUpperCase().includes("PAMELA SOUZA")) {
+            client.sendMessage("120363039562349093@g.us", msg.selectedRowId);
+          }
+
         }
-
-
-        if (msg.selectedRowId.toUpperCase().includes("BEATRIZ NALIM")) {
-          client.sendMessage("120363038291296660@g.us", msg.selectedRowId);
+        else if (msg.body == "Não") {
+          msg.reply('Ok, agradeço pelo retorno, surgindo novo paciente próximo a sua área de atendimento entraremos em contato😃')
         }
-
-        if (msg.selectedRowId.toUpperCase().includes("BIANCA NASCIMENTO")) {
-          client.sendMessage("120363039945172091@g.us", msg.selectedRowId);
-        }
-
-        if (msg.selectedRowId.toUpperCase().includes("THAIS RODRIGUES")) {
-          client.sendMessage("120363024013484590@g.us", msg.selectedRowId);
-        }
-
-        if (msg.selectedRowId.toUpperCase().includes("JULIANA SOUZA")) {
-          client.sendMessage("120363040378100634@g.us", msg.selectedRowId);
-        }
-        if (msg.selectedRowId.toUpperCase().includes("ANNE OLIVEIRA")) {
-          client.sendMessage("120363039377213562@g.us", msg.selectedRowId);
-        }
-        if (msg.selectedRowId.toUpperCase().includes("ANNA BEATRIZ")) {
-          client.sendMessage("120363038774243623@g.us", msg.selectedRowId);
-        }
-        if (msg.selectedRowId.toUpperCase().includes("BENE SOUZA")) {
-          client.sendMessage("120363023390637872@g.us", msg.selectedRowId);
-        }
-        if (msg.selectedRowId.toUpperCase().includes("HAYNE SEJANI")) {
-          client.sendMessage("120363023276101126@g.us", msg.selectedRowId);
-        }
-        if (msg.selectedRowId.toUpperCase().includes("PAMELA SOUZA")) {
-          client.sendMessage("120363039562349093@g.us", msg.selectedRowId);
-        }
-//
-
-        // console.log("entrou no sim ")
-        // msg.reply('Ok, vamos te passar maiores informações sobre o paciente😃')
-        // console.log("passou aqui")
-        // if (msg.selectedRowId.toUpperCase().includes("LARISSA DANTAS") || msg.selectedRowId.toUpperCase().includes("ISADORA RIBEIRO") || msg.selectedRowId.toUpperCase().includes("LAURA LUPPI")) {
-        //   client.sendMessage("5517981071145@c.us", msg.selectedRowId);
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("ANNE")) {
-        //   client.sendMessage("5517981520077@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("BEATRIZ NALIM") || msg.selectedRowId.toUpperCase().includes("JULIANA SOUZA") || msg.selectedRowId.toUpperCase().includes("THAIS ALVES") || msg.selectedRowId.toUpperCase().includes("ANNA BEATRIZ")) {
-        //   client.sendMessage("5517981120533@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("BIANCA NASCIMENTO") || msg.selectedRowId.toUpperCase().includes("THAIS RODRIGUES") || msg.selectedRowId.toUpperCase().includes("THAIS ALVES")) {
-        //   client.sendMessage("5517996144442@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("TAYNA SCARANELLO") || msg.selectedRowId.toUpperCase().includes("HAYNE SEJANI") || msg.selectedRowId.toUpperCase().includes("BENE SOUZA")) {
-        //   client.sendMessage("5517996248401@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("NETO FERREIRA")) {
-        //   client.sendMessage("5517981330707@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-        // }
-        // if (msg.selectedRowId.toUpperCase().includes("WILLIAN ROSA")) {
-        //   client.sendMessage("5517982080440@c.us", msg.selectedRowId);
-        //   client.sendMessage("120363022690336998@g.us", msg.selectedRowId);
-
-        // }
+      } else {
+        msg.reply('Sou uma inteligência artificial, não entendi sua mensagem por favor selecione o botão enviado pelo operador!😃')
       }
-      else if (msg.body == "Não") {
-        msg.reply('Ok, agradeço pelo retorno, surgindo novo paciente próximo a sua área de atendimento entraremos em contato😃')
-
-      }
-    }else{
-      // msg.reply('Sou uma inteligência artificial, não entendi sua mensagem por favor selecione o botão enviado pelo operador!😃')
-
 
     }
-    //  else {
-    //   // const chat = client.getChatById(msg.from).then(response =>{
-    //   //   var messages = response.fetchMessages().then(response =>{
-    //   //     console.log(messages.body);
-    //   //   });
 
-    //   // });
-    //   let list = [];
-    //   const chats = await client.getChats()
-    //   const messages = await chats[0].fetchMessages();
-    //   for (const obj of messages) {
-    //     console.log(obj.author)
-    //     if (obj.type == "list") {
-    //       list.push(obj);
-    //     }
-    //   }
-    //   if (list.length > 0) {
-    //     let msgobj = list.pop();
- 
-    //     msg.reply('Ok, vou passar sua mensagem para um de nossos analista 😃')
-
-    //     client.sendMessage("120363022690336998@g.us", msgobj.selectedRowId);
-
-    //     if (msg.selectedRowId.toUpperCase().includes("THAIS ALVES")) {
-    //       client.sendMessage("120363022690336998@g.us", msgobj.selectedRowId);
-    //     }
-
-
-    //     if (msg.selectedRowId.toUpperCase().includes("BEATRIZ NALIN")) {
-    //       client.sendMessage("120363038291296660@g.us", msgobj.selectedRowId);
-    //     }
-
-    //     if (msg.selectedRowId.toUpperCase().includes("BIANCA NASCIMENTO")) {
-    //       client.sendMessage("120363039945172091@g.us", msgobj.selectedRowId);
-    //     }
-
-    //     if (msg.selectedRowId.toUpperCase().includes("THAIS RODRIGUES")) {
-    //       client.sendMessage("120363024013484590@g.us", msgobj.selectedRowId);
-    //     }
-
-    //     if (msg.selectedRowId.toUpperCase().includes("JULIANA SOUZA")) {
-    //       client.sendMessage("120363040378100634@g.us", msgobj.selectedRowId);
-    //     }
-
-    //     if (msg.selectedRowId.toUpperCase().includes("ANNE OLIVEIRA")) {
-    //       client.sendMessage("120363039377213562@g.us", msgobj.selectedRowId);
-    //     }
-
-
-    //     if (msg.selectedRowId.toUpperCase().includes("ANNA BEATRIZ")) {
-    //       client.sendMessage("120363038774243623@g.us", msgobj.selectedRowId);
-    //     }
-    //     if (msg.selectedRowId.toUpperCase().includes("BENE SOUZA")) {
-    //       client.sendMessage("120363023390637872@g.us", msgobj.selectedRowId);
-    //     }
-    //     if (msg.selectedRowId.toUpperCase().includes("HAYNE SEJANI")) {
-    //       client.sendMessage("120363023276101126@g.us", msgobj.selectedRowId);
-    //     }
-    //   }
-    // }
   });
 
 
@@ -438,7 +344,7 @@ app.post('/send-button', (req, res) => {
   const sender = req.body.sender;
 
   const client = sessions.find(sess => sess.id == sender).client;
-   client.sendMessage(number, new List(' ', 'Clique aqui para selecionar', [{ title: 'Selecione a ação desejada', rows: [{ id: id, title: 'Sim', description: '' }, { title: 'Não' }] }], 'Selecione SIM ou NÃO', ''), { caption: '' }).then(response => {
+  client.sendMessage(number, new List(' ', 'Clique aqui para selecionar', [{ title: 'Selecione a ação desejada', rows: [{ id: id, title: 'Sim', description: '' }, { title: 'Não' }] }], 'Selecione SIM ou NÃO', ''), { caption: '' }).then(response => {
     res.status(200).json({
       status: true,
       response: response
